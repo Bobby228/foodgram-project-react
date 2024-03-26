a13c9e690045cc747b148da09f8a40c40f73f4df
Максим Бобров <bobrovmx@gmail.com>
2024-03-27 00:13:11 +0300

    Help me 2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure123456')
 
 DEBUG = True
-
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split()
 
