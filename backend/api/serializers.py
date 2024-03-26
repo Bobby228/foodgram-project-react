87e79138ca388a8ec8a1152f05cfde6e801b2cf4
Максим Бобров <bobrovmx@gmail.com>
2024-03-26 21:17:52 +0300

    idk

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -13,12 +13,12 @@ from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
 from users.models import CustomUser, Follow
 
 
-def get_data_from_model(self, obj, model):
-    request = self.context.get('request')
-    if request is None or request.user.is_anonymous:
-        return False
-    user = request.user
-    return model.objects.filter(recipe=obj, user=user).exists()
+# def get_data_from_model(self, obj, model):
+#     request = self.context.get('request')
+#     if request is None or request.user.is_anonymous:
+#         return False
+#     user = request.user
+#     return model.objects.filter(recipe=obj, user=user).exists()
 
 
 class CustomUserSerializer(UserCreateSerializer):
@@ -160,21 +160,21 @@ class RecipeReadSerializer(serializers.ModelSerializer):
 
     def get_is_favorited(self, obj):
         """Получение избранных рецептов."""
-        return get_data_from_model(self=self, obj=obj, model=Favorite)
-        # request = self.context.get('request')
-        # if request is None or request.user.is_anonymous:
-        #     return False
-        # user = request.user
-        # return Favorite.objects.filter(recipe=obj, user=user).exists()
+        # return get_data_from_model(self=self, obj=obj, model=Favorite)
+        request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
+        user = request.user
+        return Favorite.objects.filter(recipe=obj, user=user).exists()
 
     def get_is_in_shopping_cart(self, obj):
         """Получение списка покупок."""
-        return get_data_from_model(self=self, obj=obj, model=ShoppingCart)
-        # request = self.context.get('request')
-        # if request is None or request.user.is_anonymous:
-        #     return False
-        # user = request.user
-        # return ShoppingCart.objects.filter(recipe=obj, user=user).exists()
+        # return get_data_from_model(self=self, obj=obj, model=ShoppingCart)
+        request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
+        user = request.user
+        return ShoppingCart.objects.filter(recipe=obj, user=user).exists()
 
 
 class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
